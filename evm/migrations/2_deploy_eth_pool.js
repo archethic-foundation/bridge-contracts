fdc8089ff2b6d225f619f84c4729560c2d712146
Neylix <julien.leclerc05@protonmail.com>
2023-10-10 20:54:39 +0200

    Update archethic pool signer

--- evm/migrations/2_deploy_eth_pool.js
+++ evm/migrations/2_deploy_eth_pool.js
@@ -2,7 +2,7 @@ const LiquidityPool = artifacts.require("ETHPool")
 
 const { deployProxy } = require('@openzeppelin/truffle-upgrades');
 
-module.exports = async function (deployer, network, accounts) {
+module.exports = async function(deployer, network, accounts) {
     let reserveAddress, safetyModuleAddress, archethicPoolSigner, poolCap
     const safetyModuleFeeRate = 5 // 0.05%
     const lockTimePeriod = 7200; // 2H
@@ -10,7 +10,7 @@ module.exports = async function (deployer, network, accounts) {
     if (network == "development") {
         reserveAddress = accounts[4]
         safetyModuleAddress = accounts[5]
-        archethicPoolSigner = '0x480b332bf0cb7b6f225b06a16ea2a27c74379f9d'
+        archethicPoolSigner = '0x200066681c09a9a8c9352fac9b96a688a4ae0b39'
         poolCap = web3.utils.toWei('200')
     }
 
@@ -18,26 +18,22 @@ module.exports = async function (deployer, network, accounts) {
         reserveAddress = "0x3FDf8f04cBe76c1376F593634096A5299B494678"
         safetyModuleAddress = "0x57B5Fe2F6A28E108208BA4965d9879FACF629442"
         poolCap = web3.utils.toWei('5')
-        archethicPoolSigner = '0xaf08762b5c7001314dca6e9c3aa56c1a603f9369'
+        archethicPoolSigner = '0x28c9efc42e2cbdfb581c212fe1e918a480ca1421'
     }
 
     if (network == "mumbai") {
         reserveAddress = "0x64d75D315c592cCE1F83c53A201313C82b30FA8d"
         safetyModuleAddress = "0xc20BcA1a8155c65964e5280D93d379aeB3A4c2e7"
         poolCap = web3.utils.toWei('5')
-        archethicPoolSigner = '0xaf08762b5c7001314dca6e9c3aa56c1a603f9369'
+        archethicPoolSigner = '0x4e57f0bf5813f5a516d23a59df1c767c4a3e8eef'
     }
 
     if (network == "bsc_testnet") {
         reserveAddress = "0x7F9E1c2Bb1Ab391bA9987070ED8e7db77A9c8818"
         safetyModuleAddress = "0x6f3dec2738b063D9aFe4436b1ec307D84f9C2EDe"
         poolCap = web3.utils.toWei('5')
-        archethicPoolSigner = '0xaf08762b5c7001314dca6e9c3aa56c1a603f9369'
+        archethicPoolSigner = '0x461ac2fa849767e4059fd98903a61315434ccf64'
     }
 
-    const instance = await deployProxy(LiquidityPool, [reserveAddress, safetyModuleAddress, safetyModuleFeeRate, archethicPoolSigner, poolCap, lockTimePeriod], { deployer });
-
-    if (network == "development") {
-        
-    }
+    await deployProxy(LiquidityPool, [reserveAddress, safetyModuleAddress, safetyModuleFeeRate, archethicPoolSigner, poolCap, lockTimePeriod], { deployer });
 }
--- evm/migrations/3_deploy_erc_pool.js
+++ evm/migrations/3_deploy_erc_pool.js
@@ -3,7 +3,7 @@ const DummyToken = artifacts.require("DummyToken")
 
 const { deployProxy } = require('@openzeppelin/truffle-upgrades');
 
-module.exports = async function (deployer, network, accounts) {
+module.exports = async function(deployer, network, accounts) {
     let reserveAddress, safetyModuleAddress, archethicPoolSigner, poolCap, tokenAddress
     const safetyModuleFeeRate = 5 // 0.05%
     const lockTimePeriod = 7200; //2H
@@ -11,7 +11,7 @@ module.exports = async function (deployer, network, accounts) {
     if (network == "development") {
         reserveAddress = accounts[4]
         safetyModuleAddress = accounts[5]
-        archethicPoolSigner = '0xb2ebd20cc1b50bd1c68e84f1148a304fff15706c'
+        archethicPoolSigner = '0xcb2276e4760757976438922aaeb0e03114d5b45f'
         poolCap = web3.utils.toWei('200')
 
         await deployer.deploy(DummyToken, web3.utils.toWei('200000'))
@@ -26,7 +26,7 @@ module.exports = async function (deployer, network, accounts) {
         reserveAddress = "0x4c5B45aD4347bAAF2E2d1817D0e1eea483910acc"
         safetyModuleAddress = "0xbEF25E2b992494aF270092562e92aAC8394e0982"
         poolCap = web3.utils.toWei('5')
-        archethicPoolSigner = '0xaf08762b5c7001314dca6e9c3aa56c1a603f9369'
+        archethicPoolSigner = '0x85d7e244e533c7c71ef80f9a56fd1115bb9e5c69'
         tokenAddress = '0xCBBd3374090113732393DAE1433Bc14E5233d5d7'
     }
 
@@ -34,7 +34,7 @@ module.exports = async function (deployer, network, accounts) {
         reserveAddress = "0x5aAD864466491E81701103F04775EA7dE6d76fE3"
         safetyModuleAddress = "0xbADc499dA1F766599d19dBa805CB62eFaa439Adc"
         poolCap = web3.utils.toWei('5')
-        archethicPoolSigner = '0xaf08762b5c7001314dca6e9c3aa56c1a603f9369'
+        archethicPoolSigner = '0x85d7e244e533c7c71ef80f9a56fd1115bb9e5c69'
         tokenAddress = '0x51279e98d99AA8D65763a885BEFA5463dCd84Af6'
     }
 
@@ -42,13 +42,9 @@ module.exports = async function (deployer, network, accounts) {
         reserveAddress = "0x157aB6F84d3a3874d1dEd1b0a63EC834C640FBda"
         safetyModuleAddress = "0x95Cc38814d37E0A9b08f77a02Ff4045CeAd2106c"
         poolCap = web3.utils.toWei('5')
-        archethicPoolSigner = '0xaf08762b5c7001314dca6e9c3aa56c1a603f9369'
+        archethicPoolSigner = '0x85d7e244e533c7c71ef80f9a56fd1115bb9e5c69'
         tokenAddress = '0x51279e98d99AA8D65763a885BEFA5463dCd84Af6'
     }
 
-    const instance = await deployProxy(LiquidityPool, [reserveAddress, safetyModuleAddress, safetyModuleFeeRate, archethicPoolSigner, poolCap, lockTimePeriod, tokenAddress], { deployer });
-
-    if (network == "development") {
-        
-    }
+    await deployProxy(LiquidityPool, [reserveAddress, safetyModuleAddress, safetyModuleFeeRate, archethicPoolSigner, poolCap, lockTimePeriod, tokenAddress], { deployer });
 }
