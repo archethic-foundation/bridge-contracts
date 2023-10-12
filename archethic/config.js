672d046f5eae5da6c18f9ef580db593f8e7758d3
Neylix <julien.leclerc05@protonmail.com>
2023-10-12 16:49:08 +0200

    Implement chargeable call HTTP control

--- archethic/config.js
+++ archethic/config.js
@@ -14,13 +14,17 @@ export default {
     local: {
       endpoint: "http://127.0.0.1:8545",
       chainId: 31337,
-      tokenAddresses: {
-        UCO: "0xc944370f51eda8179b5180f5ab512134540939e3",
-        aeETH: "NATIVE"
-      },
-      proxyAddresses: {
-        UCO: "0x24e57fc6cfb7f67928e32aee3e3ff98f0e968a5d",
-        aeETH: "0x26f8c6db23a4aa5293eeeee8a3317773e849cf44"
+      tokens: {
+        UCO: {
+          tokenAddress: "0xc944370f51eda8179b5180f5ab512134540939e3",
+          proxyAddress: "0x24e57fc6cfb7f67928e32aee3e3ff98f0e968a5d",
+          decimals: 18
+        },
+        aeETH: {
+          tokenAddress: "NATIVE",
+          proxyAddress: "0x26f8c6db23a4aa5293eeeee8a3317773e849cf44",
+          decimals: 18
+        }
       }
     },
     sepolia_ethereum: {
