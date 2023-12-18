3a45a529b9a999cbecd9805f1aaf6b60a73a8fe4
Samuel Manzanera <samuelmanzanera@protonmail.com>
2023-12-18 10:02:40 +0100

    Improve analytics tool for EVM

--- evm/scripts/analytics/uco_bridged.js
+++ evm/scripts/analytics/uco_bridged.js
@@ -1,13 +1,13 @@
-const { ethers }  = require("hardhat");
-
+const { ethers } = require("hardhat");
+const bars = require('bars');
 
 async function main() {
   const network = process.env["HARDHAT_NETWORK"]
 
-  console.log(`Fetching cumul of UCO bridged from pools for ${network}...`)
+  console.log(`Fetching UCO swaps from pools for ${network}...`)
 
   let poolAddr
-  switch(network) {
+  switch (network) {
     case "sepolia":
       poolAddr = "0x50b8B73327613468e5605eD59B980555DAAC354a"
       break
@@ -21,56 +21,89 @@ async function main() {
   }
 
   const pool = await getPoolContract(poolAddr)
-  const { total: chargeableAmount, count: nbChargeableSwaps} = await getChargeableCumul(pool)
-  const { total: signedAmount, count: nbSignedSwaps} = await getSignedCumul(pool)
+
+  const { total: chargeableAmount, count: nbChargeableSwaps, nbRefunds: nbRefundChargeable, nbIncompleted: nbIncompletedChargeable, history: chargeabelHistory } = await getChargeableStats(pool)
+  const { total: signedAmount, count: nbSignedSwaps, nbRefunds: nbRefundSigned, nbIncompleted: nbIncompletedSigned, history: signedHistory } = await getSignedStats(pool)
+
 
   console.log("------------------------------------------")
-  console.log(`Cumul UCO bridged ${network}->Archethic: ${chargeableAmount} (for ${nbChargeableSwaps} swaps)`)
-  console.log(`Cumul UCO bridged Archethic->${network}: ${signedAmount} (for ${nbSignedSwaps} swaps)`)
+  console.log(`${network} => Archethic`)
   console.log("------------------------------------------")
+  console.log(`Cumul UCO bridged: ${chargeableAmount} (for ${nbChargeableSwaps} swaps)`)
+  console.log(`Number of refunded swaps: ${nbRefundChargeable}`)
+  console.log(`Number of incomplete swaps: ${nbIncompletedChargeable}`)
+  console.log("---")
+  console.log('History')
+  console.log(bars(chargeabelHistory, { bar: '=', width: 20 }))
+
+  console.log("")
+  console.log("------------------------------------------")
+  console.log(`Archethic => ${network}`)
+  console.log("------------------------------------------")
+  console.log(`Cumul UCO bridged: ${signedAmount} (for ${nbSignedSwaps} swaps)`)
+  console.log(`Number of refunded swaps: ${nbRefundSigned}`)
+  console.log(`Number of incomplete swaps: ${nbIncompletedSigned}`)
+  console.log("---")
+  console.log('History')
+  console.log(bars(signedHistory, { bar: '=', width: 20 }))
 }
 
-async function getChargeableCumul(pool) {
+async function getChargeableStats(pool) {
   const swapsAddresses = await pool.mintedSwaps()
-  return await getCumulAmount(swapsAddresses, "ChargeableHTLC_ERC")
+  const swaps = await fetchSwaps(swapsAddresses, "ChargeableHTLC_ERC")
+  return getStats(swaps)
 }
 
-async function getSignedCumul(pool) {
+async function getSignedStats(pool) {
   const swapsAddresses = await pool.provisionedSwaps()
-  return await getCumulAmount(swapsAddresses, "SignedHTLC_ERC")
+  const swaps = await fetchSwaps(swapsAddresses, "SignedHTLC_ERC")
+  return getStats(swaps)
 }
 
 async function getPoolContract(address) {
   return await ethers.getContractAt("ERCPool", address)
 }
 
-async function getCumulAmount(swapsAddresses, contractType) {
-  promises = await Promise.all(swapsAddresses.map(async (swapAddress) => {
+async function fetchSwaps(addresses, contractType) {
+  return await Promise.all(addresses.map(async (swapAddress) => {
     const htlc = await ethers.getContractAt(contractType, swapAddress)
-    const htlcStatus = await htlc.status()
-    if (htlcStatus == 1) {
-      const htlcAmount = await htlc.amount()
-      return htlcAmount
-    } else {
-      return 0
+    return {
+      status: await htlc.status(),
+      lockTime: await htlc.lockTime(),
+      amount: new Number(ethers.formatEther(await htlc.amount()))
     }
   }))
-
-
-  let bridgedAmount = 0.0
-  promises.forEach(amount => {
-    bridgedAmount += new Number(ethers.formatEther(amount))
-  })
-
-  return { total: bridgedAmount, count: swapsAddresses.length }
-
 }
 
+function getStats(swaps) {
+  let bridgedAmount = 0.0
+  let nbRefunds = 0
+  let nbIncompleted = 0
+  let nbSwaps = 0
+  let history = {}
+
+  swaps.forEach(({ status, amount, lockTime }) => {
+    if (status == 0) {
+      nbIncompleted++
+    }
+    if (status == 2) {
+      nbRefunds++
+    }
+    bridgedAmount += amount
+    nbSwaps++
+
+    const date = new Date(lockTime.toString() * 1000)
+    const day = date.toISOString().split('T')[0]
+    history[day] ? history[day] += 1 : history[day] = 1
+  })
+
+  return { total: bridgedAmount, count: nbSwaps, nbRefunds, nbIncompleted, history }
+}
 
 main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error);
-        process.exit(1)
-    });
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1)
+  });
 
