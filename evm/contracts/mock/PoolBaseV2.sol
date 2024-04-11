9ccc85313621a7937122d200ca4b2b9b60e728c6
Samuel Manzanera <samuelmanzanera@protonmail.com>
2024-04-11 15:36:08 +0200

    lint

--- evm/contracts/mock/PoolBaseV2.sol
+++ evm/contracts/mock/PoolBaseV2.sol
@@ -3,8 +3,8 @@ pragma solidity 0.8.21;
 
 import "../Pool/PoolBase.sol";
 
-contract PoolBaseV2 is PoolBase {
-    
+abstract contract PoolBaseV2 is PoolBase {
+
     function setSafetyModuleFeeRate(uint256 _safetyFeeRate) onlyOwner override external {
         safetyModuleFeeRate = _safetyFeeRate * 2;
         emit SafetyModuleFeeRateChanged(_safetyFeeRate * 2);
